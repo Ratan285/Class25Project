fa4fe9d55369942057812fde791845498d13cced
Ratan285 <ratanprasanna3@gmail.com>
2021-03-25 14:56:16 +0530

    changes 1

--- sketch.js
+++ sketch.js
@@ -10,32 +10,32 @@ var world;
 function setup() {
 	createCanvas(1600, 700);
 	rectMode(CENTER);
+   
 
 
 	engine = Engine.create();
 	world = engine.world;
-	
+
+	paper = new Paper(50,50);
 	groundObject=new ground(width/2,670,width,20);
 	dustbinObj=new dustbin(1200,650);
-    paper = new Paper(50,50);
+   
 
 	Engine.run(engine);
-  
 }
 
 
 function draw() {
-  rectMode(CENTER);
   background(230);
 
-  //paper.y = paper.y+1;
-  if(keyDown(DOWN_ARROW)){
-	  paper.velocityX=+10;
-	  paper.velocityY = +5;
-  }
   groundObject.display();
   dustbinObj.display();
   paper.display();
 
+}
+function keyPressed(){
+	if(keyCode = 40){
+	   Matter.Body.applyForce(paper,20,5);
+	}   
 }
 
